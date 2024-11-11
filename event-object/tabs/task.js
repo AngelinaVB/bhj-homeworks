eb23c97bf978924556c1815983458038e0fed08f
AngelinaVB <b.lina.1995@mail.ru>
2024-11-11 23:44:10 +0300

    three tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,15 +1,19 @@
 const elemTab = document.getElementsByClassName('tab');
 const arrElemTab = Array.from(elemTab);
 const elemContentTab = document.getElementsByClassName('tab__content');
+const arrElemContentTab = Array.from(elemContentTab);
+const activeElem = document.getElementsByClassName('tab_active');
+const arrActiveElem = Array.from(activeElem);
 
-console.log(elemTab);
-console.log(elemContentTab);
-
-let indexTab = arrElemTab.indexOf('');
-
-elemTab[indexTab].addEventListener('click', function()
+arrElemTab.forEach(arrElemTab => {arrElemTab.addEventListener('click', function()
 { 
-  elemTab.classList.add('tab_active');
+  if (arrElemTab.indexOf(activeElem) == -1) {
+    return;
+  }
+  else {
+  arrActiveElem.classList.remove('tab_active');
+  arrElemTab.classList.add('tab_active');
+  arrElemContentTab.classList.add('tab__content_active');
+  };
 });
-
-console.log(elemTab[0].textContent);
+});
