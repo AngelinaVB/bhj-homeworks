13d500aa97671e3deb1fdeac6fa812ecfb9f1459
AngelinaVB <b.lina.1995@mail.ru>
2024-11-19 00:27:21 +0300

    ready dropdowm

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -4,19 +4,19 @@ let itemValue = document.getElementsByClassName('dropdown__item');
 let itemValueArr = Array.from(itemValue);
 
 selectValue[0].addEventListener('click', function () {
-    listValues.classList.toggle('dropdown__list_active');
+  listValues.classList.toggle('dropdown__list_active');
 });
 
 for (let i = 0; i < itemValueArr.length; i++) {
-    itemValueArr[i].addEventListener('click', function (e) {
-        const targetElem = e.target;
-        const item = targetElem.closest('.dropdown__item')
-        if (item === null) {
-          return;
-        } else {
-          e.preventDefault();
-          selectValue[0].textContent = itemValueArr[i].textContent;
-          listValues.classList.toggle('dropdown__list_active');
-        }
-    })
+  itemValueArr[i].addEventListener('click', function (e) {
+    const targetElem = e.target;
+    const item = targetElem.closest('.dropdown__item')
+    if (item === null) {
+      return;
+    }
+    e.preventDefault();
+    selectValue[0].textContent = itemValueArr[i].textContent;
+    listValues.classList.toggle('dropdown__list_active');
+
+  })
 }
