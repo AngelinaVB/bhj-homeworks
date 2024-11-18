72cd12e67d277777e6a2b44d3db205eb4f6c9af0
AngelinaVB <b.lina.1995@mail.ru>
2024-11-19 00:28:21 +0300

    частично готово

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,15 +2,24 @@ const elemTab = document.querySelectorAll('.tab');
 const arrElemTab = Array.from(elemTab);
 const elemContentTab = document.querySelectorAll('.tab__content');
 const arrElemContentTab = Array.from(elemContentTab);
-const activeElem = document.querySelector('.tab_active');
-const activeElemContent = document.querySelector('.tab__content_active');
+
 
 arrElemTab.forEach((elem, index) => {
+  const activeElem = document.querySelectorAll(`.tab_active${index}`);
+  const arrActiveElem = Array.from(activeElem);
+  const activeElemContent = document.querySelectorAll('.tab__content_active');
+  const arrActiveElemContent = Array.from(activeElemContent);
+
   elem.addEventListener('click', function () {
+    let indexActive = arrElemTab.findIndex((arrActiveElem) => arrActiveElem);
+    console.log(arrElemTab);
+    console.log(indexActive);
+
+    arrElemTab[indexActive].classList.remove('tab_active');
+    arrElemContentTab[indexActive].classList.remove('tab__content_active');
+
     arrElemTab[index].classList.add('tab_active');
     arrElemContentTab[index].classList.add('tab__content_active');
-    activeElem.classList.remove('tab_active');
-    activeElemContent.classList.remove('tab__content_active');
-    return false;
+
   });
 });
