99cef3d55b833d32a3709a65e24604fe7aa2235f
AngelinaVB <b.lina.1995@mail.ru>
2025-02-13 23:04:35 +0300

    частично готово первое задание

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,17 +1,18 @@
 
 const elem = document.querySelectorAll('.reveal');
-const elemArray = Array.from(elem);
+const rect = elem[0].getBoundingClientRect().bottom;
+const rect1 = elem[1].getBoundingClientRect().bottom;
 
-elemArray.forEach((index) => {
 document.addEventListener('scroll', function () {
-    const scrollPosition = document.documentElement.scrollTop;
-    const rect = elemArray[index].getBoundingClientRect();
-    if (scrollPosition == rect) {
-        elemArray[index].classList.add('reveal_active');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition <= rect || scrollPosition <= rect1) {
+        elem[0].classList.add('reveal_active');
+        elem[1].classList.add('reveal_active');
     }
     else {
-        elemArray[index].classList.remove('reveal_active');
+        elem[0].classList.remove('reveal_active');
+        elem[1].classList.remove('reveal_active');
     }
-});
 });
 
