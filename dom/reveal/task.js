96d3d646235773155ae6b0630b5901711b3f82bc
AngelinaVB <b.lina.1995@mail.ru>
2025-03-02 22:55:43 +0300

    первое задание готово

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,18 +1,16 @@
-
-const elem = document.querySelectorAll('.reveal');
-const rect = elem[0].getBoundingClientRect().bottom;
-const rect1 = elem[1].getBoundingClientRect().bottom;
-
-document.addEventListener('scroll', function () {
+const elemActive = document.querySelectorAll('.reveal');
+const elemArray = Array.from(elemActive);
+    
+elemArray.forEach((elem) => {
+    document.addEventListener('scroll', function () {
     const scrollPosition = window.scrollY;
-
-    if (scrollPosition <= rect || scrollPosition <= rect1) {
-        elem[0].classList.add('reveal_active');
-        elem[1].classList.add('reveal_active');
+    const rect = elem.getBoundingClientRect().bottom;
+  
+    if (scrollPosition <= rect) {
+        elem.classList.remove('reveal_active');
     }
     else {
-        elem[0].classList.remove('reveal_active');
-        elem[1].classList.remove('reveal_active');
+        elem.classList.add('reveal_active');
     }
 });
-
+});
