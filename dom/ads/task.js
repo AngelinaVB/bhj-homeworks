3452d12317ee5216786e82c38f69b2495f59f542
AngelinaVB <b.lina.1995@mail.ru>
2025-03-06 21:46:12 +0300

    третье задание готово

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,24 +1,14 @@
-const b = Array.from(document.querySelectorAll('.rotator__case'))
-const a = Array.from(document.querySelectorAll('.rotator'));
-
-console.log(a);
-console.log(b);
-
+const rotatorArray = Array.from(document.querySelectorAll('.rotator__case'))
 
 function intervalFunc() {
-    b.forEach((elem, index) => {
-        console.log(index);
-        console.log(elem);
-        function indexActive(element) {return element.classList.contains('rotator__case_active') };
-        let idElem = b.findIndex(indexActive);
-        const randomIndex = Math.floor(Math.random() * (b.length - 1));
+    rotatorArray.forEach((elem, index) => {
+        function indexActive(element) { return element.classList.contains('rotator__case_active') };
+        let idElem = rotatorArray.findIndex(indexActive);
+        const randomIndex = Math.floor(Math.random() * (rotatorArray.length - 1));
 
-        b[idElem].classList.remove('rotator__case_active');
-
-
-        b[randomIndex].classList.add('rotator__case_active');
+        rotatorArray[idElem].classList.remove('rotator__case_active');
+        rotatorArray[randomIndex].classList.add('rotator__case_active');
     });
 };
-
 
 setInterval(intervalFunc, 1000);
