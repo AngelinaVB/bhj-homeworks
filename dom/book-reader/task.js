b085bb7a4928332d13b3fc0c7a3b7f7f6aee4d53
AngelinaVB <b.lina.1995@mail.ru>
2025-03-06 21:40:43 +0300

    второе задание частично готово

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,27 +1,22 @@
-
-const a = document.getElementById('book');
+const mainPage = document.getElementById('book');
 const textFontSize = Array.from(document.querySelectorAll('.font-size'));
-
 const dataElements = document.querySelectorAll('[data-size]');
 
-console.log(a);
-console.log(textFontSize);
-console.log(dataElements[0]);
-
 textFontSize.forEach((elem, index) => {
-elem.addEventListener('click', function () {
-    function indexActive(element) { return element.classList.contains('font-size_active') };
-    let idElem = textFontSize.findIndex(indexActive);
-   
-    textFontSize[idElem].classList.remove('font-size_active');
-    textFontSize[index].classList.remove('font-size_small');
-    textFontSize[index].classList.add('font-size_active')
+    elem.addEventListener('click', function (event) {
+        event.preventDefault();
 
-    if (idElem == dataElements[0]) { 
-       a.classList.remove('book_fs-small'); 
-       a.classList.add('book_fs-small');
-    }
-    else {a.classList.add('book_fs-big')};
-   
-});  
+        function indexActive(element) { return element.classList.contains('font-size_active') };
+        let idElem = textFontSize.findIndex(indexActive);
+
+        textFontSize[idElem].classList.remove('font-size_active');
+        textFontSize[index].classList.add('font-size_active');
+
+        if (elem == dataElements[1]) {
+            mainPage.classList.add('book_fs-big');
+        }
+        if (elem == dataElements[0]) {
+            mainPage.classList.add('book_fs-small');
+        }
+    });
 });
